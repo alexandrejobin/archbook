ad1a55c90107273d006a3780bac00cbc68f8a1be
Alexandre Jobin <alex.jobin@hotmail.com>
2018-03-16 09:26:37 -0400

    Disable client side validation for MVC Core

--- src/ArchBook.Web2/Startup.cs
+++ src/ArchBook.Web2/Startup.cs
@@ -38,7 +38,13 @@ namespace ArchBook.Web2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services
+                .AddMvc()
+                .AddViewOptions(setup =>
+                {
+                    setup.HtmlHelperOptions.ClientValidationEnabled = false;
+                });
+
             services.AddRazorViewRenderer();
 
             services.AddScoped<BookDbContext>(_ => new BookDbContext(Configuration.GetConnectionString("BookConnectionString")));
